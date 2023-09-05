5cb187f2a14b11c8e85da1c3388381b8a1e2f28f
RONALDO UCHOA <ronaldo.uchoa.pessoal@gmail.com>
2023-09-05 14:29:42 -0300

    feat: config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListingComponent } from './components/posts/post-listing/post-listing.component';
+import { PostComponent } from './components/posts/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'postListing',
+    pathMatch: 'full'
+  },
+  {
+    path: 'postListing',
+    component: PostListingComponent
+  },
+  {
+    path: 'post/:id',
+    component: PostComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'postListing'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PostListingComponent } from './components/posts/post-listing/post-listing.component';
+import { PostComponent } from './components/posts/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListingComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
